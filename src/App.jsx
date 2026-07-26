76d1c38ea60e9d30354ac46741fa867963a33dbd
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-07-26 15:38:44 -0400

    add modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useEffect, useState } from 'react';
 import { getAllStarships } from "./services/sw-api";
 import StarshipCard from "./components/StarshipCard";
-export default function App() {
+      function App() {
      const [starships, setStarships] = useState([]);
+      useEffect(() => {
+    async function fetchStarships() {
+      try {
+        const data = await getAllStarships();
+        setStarships(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    fetchStarships();
+  }, []);
+
   return (
     <main>
       <h1>Star Wars Starships</h1>
+      {starships.map((starship) => (
+        <StarshipCard
+          key={starship.url}
+          starship={starship}
+        />
+      ))}
     </main>
   );
 }
--- src/services/sw-api.jsx
+++ src/services/sw-api.jsx
@@ -1,9 +1,9 @@
 const BASE_URL = "https://swapi.dev/api";
 
 export async function getAllStarships() {
-  const res = await fetch(`${BASE_URL}/starships/`);
+  const response = await fetch(`${BASE_URL}/starships/`);
 
-  if (!res.ok) {
+  if (!response.ok) {
     throw new Error("Failed to fetch starships");
   }
 
